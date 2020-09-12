f5a302dba34be6f855f27ec34e7f2a86f570727a
dpankov91 <dpankov91@gmail.com>
2020-09-12 11:08:23 +0200

    finished owners core

---
+++ PetShop.Core/ApplicationService/Services/IOwnerService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PetShop.Core.ApplicationService.Services
+{
+    interface IOwnerService
+    {
+    }
+}
---
+++ PetShop.Core/ApplicationService/Services/Implementation/OwnerService.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using PetShop.Core.Entity;
+
+namespace PetShop.Core.ApplicationService.Services.Implementation
+{
+    public class OwnerService : IOwnerService
+    {
+        private IOwnerService _ownerService;
+
+        public OwnerService(IOwnerService ownerService)
+        {
+            _ownerService = ownerService;
+        }
+        public List<Owner> GetAllOwners()
+        {
+           return _ownerService.GetAllOwners();
+        }
+
+        public Owner FindOwnerById(int id)
+        {
+            return _ownerService.FindOwnerById(id);
+        }
+
+        public Owner Create(Owner owner)
+        {
+            return _ownerService.Create(owner);
+        }
+
+        public Owner CreateNewOwner(string FirstName, string SecondName, int Age, string Address, int PhoneNumber)
+        {
+            Owner owner = new Owner()
+            {
+                FirstName = FirstName,
+                SecondName = SecondName,
+                Age = Age,
+                Address = Address,
+                PhoneNumber = PhoneNumber
+            };
+            _ownerService.Create(owner);
+            return owner;
+
+        }
+
+        public Owner Delete(int id)
+        {
+            return _ownerService.Delete(id);
+        }
+
+
+        public Owner Update(Owner ownerToUpdate)
+        {
+            Owner owner = FindOwnerById(ownerToUpdate.Id);
+
+            owner.FirstName = ownerToUpdate.FirstName;
+            owner.SecondName = ownerToUpdate.SecondName;
+            owner.Age = ownerToUpdate.Age;
+            owner.Address = ownerToUpdate.Address;
+            owner.PhoneNumber = ownerToUpdate.PhoneNumber;
+
+            return owner;
+        }
+    }
+}
