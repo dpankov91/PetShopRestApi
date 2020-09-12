74d9e9eb3180e97e546bfba63d2540e589591364
dpankov91 <dpankov91@gmail.com>
2020-09-12 11:09:17 +0200

    a

--- PetShop.Core/ApplicationService/Services/IOwnerService.cs
+++ PetShop.Core/ApplicationService/Services/IOwnerService.cs
@@ -1,10 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using PetShop.Core.Entity;
 
 namespace PetShop.Core.ApplicationService.Services
 {
-    interface IOwnerService
+    public interface IOwnerService
     {
+        List<Owner> GetAllOwners();
+
+        Owner FindOwnerById(int id);
+
+        Owner CreateNewOwner(string FirstName, string SecondName, int Age, string Address, int PhoneNumber);
+
+        Owner Create(Owner owner);
+
+        Owner Delete(int id);
+
+        Owner Update(Owner ownerToUpdate);
     }
 }
