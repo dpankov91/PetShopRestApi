1b3e4e0eff6c7fa8cc629fb0b9fff413d8ef96b6
dpankov91 <dpankov91@gmail.com>
2020-09-22 17:05:36 +0200

    filter project added

---
+++ PetShopRestApi/PetShop.Core.Filter/FilteredList.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PetShop.Core.Filter
+{
+    public class FilteredList<T>
+    {
+            public Filter FilterUsed { get; set; }
+            public int TotalCount { get; set; }
+            public List<T> List { get; set; }
+
+    }
+}
